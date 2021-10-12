bf14eb1c5a149bbb91ec4d1447520cf5138a35e5
LAPTOP-VOM3VB2L\LIZIE <elb@environmental-expert.com>
2021-10-12 11:04:28 -0500

    Agregar archivos de proyecto.

---
+++ WFCValidateEmail/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFCValidateEmail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static bool ComprobarFormatoEmail(string seMailAComprobar)
+        {
+            String sFormato;
+            sFormato = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@(([a-zA-Z]+[\\w-]+\\.){1,2}[a-zA-Z]{2,4})$";
+            if (Regex.IsMatch(seMailAComprobar, sFormato))
+            {
+                if (Regex.Replace(seMailAComprobar, sFormato, String.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ComprobarFormatoEmail(textBox1.Text) == false)
+            {
+                label2.Text = "Dirección no valida";
+                label2.ForeColor = Color.Red;
+            }
+            else
+            {
+                label2.Text = "Dirección valida";
+                label2.ForeColor = Color.Green;
+            }
+        }
+    }
+}
